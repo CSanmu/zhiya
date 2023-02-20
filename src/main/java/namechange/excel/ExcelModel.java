ce0e6f5bece0a43839848e21ea45acd49ecb6210
chenzengsen09222 <chenzengsen09222@hellobike.com>
2023-02-20 20:02:55 +0800

    sec push

--- src/main/java/namechange/excel/ExcelModel.java
+++ src/main/java/namechange/excel/ExcelModel.java
@@ -1,5 +1,26 @@
 package namechange.excel;
 
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
 
+@Data
 public class ExcelModel {
+
+    @ExcelProperty("序号")
+    private String serialNumber;
+
+    @ExcelProperty("姓名")
+    private String name;
+
+    @ExcelProperty("电话")
+    private String phone;
+
+    @ExcelProperty("地址")
+    private String address;
+
+    @ExcelProperty("尺寸")
+    private String size;
+
+    @ExcelProperty("数量")
+    private String number;
 }
--- src/main/java/namechange/excel/Read.java
+++ src/main/java/namechange/excel/Read.java
@@ -1,7 +1,28 @@
 package namechange.excel;
 
+import com.alibaba.excel.EasyExcel;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
 public class Read {
 
+    public static void read(String path) {
+
+        try {
+            InputStream inputStream = new FileInputStream(path);
+            List<ExcelModel> modelList = EasyExcel.read(inputStream).head(ExcelModel.class).sheet().headRowNumber(1).doReadSync();
 
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Read.read("/Users/chenzengsen09222/Desktop/excel.xls");
+    }
 }
