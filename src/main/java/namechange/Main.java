f5b15e3e9d500fe0a0ca77416cb751f97e10c0d9
陈增森 <zengsen5@163.com>
2023-02-22 00:03:12 +0800

    push

--- src/main/java/namechange/Main.java
+++ src/main/java/namechange/Main.java
@@ -1,9 +1,91 @@
 package namechange;
 
+import namechange.excel.ExcelModel;
+import namechange.excel.Read;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Map<Integer, String> numberMap = getMap();
+        String excelPath = "C:\\Users\\bb135\\Desktop\\代发客户排序导单模板（吱吖）(1).xls";
+        String folderPath = "C:\\Users\\bb135\\Desktop\\徐方宇 2.7";
+        Map<String, List<ExcelModel>> nameToExcelModel = Read.readExcel(excelPath);
+        Map<String, List<File>> nameToFileList = Read.readImageFolder(folderPath);
+        for (Map.Entry<String, List<File>> item : nameToFileList.entrySet()) {
+            List<ExcelModel> excelModels = nameToExcelModel.get(item.getKey());
+            List<File> value = item.getValue();
+            if (excelModels.size() == 1) {
+                ExcelModel excelModel = excelModels.get(0);
+                for (int i = 1; i <= value.size(); i++) {
+                    File file = value.get(i);
+                    String serialNumber = excelModel.getSerialNumber();
+                    String name = excelModel.getName();
+                    String[] split = excelModel.getSize().split("/*");
+                    int width = Integer.parseInt(split[0]);
+                    int high = Integer.parseInt(split[1]);
+                    String thickness = split[2];
+                    String newName = serialNumber + "-" + name + " " + width / 10 + "-" + high / 10 + " " + thickness + "mm" + "第" + numberMap.get(i) + "张";
+                    renameTo(file, newName);
+                }
+            }
+
+
+//            Map<String, ExcelModel> sizeToCountMap = excelModels.stream().collect(Collectors.toMap(r -> {
+//                String[] split = r.getSize().split("/*");
+//                int width = Integer.parseInt(split[0]);
+//                int high = Integer.parseInt(split[1]);
+//                return width / 10 + "-" + high / 10;
+//            }, t->t));
+        }
     }
 
+
+    public static void renameTo(File oldFile, String newName) {
+        String parent = oldFile.getParent();
+        String newPath = parent + File.separator + newName;
+        File newFile = new File(newPath);
+        newFile.renameTo(newFile);
+    }
+
+    public static Map<Integer, String> getMap() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "一");
+        map.put(2, "二");
+        map.put(3, "三");
+        map.put(4, "四");
+        map.put(5, "五");
+        map.put(6, "六");
+        map.put(7, "七");
+        map.put(8, "八");
+        map.put(9, "九");
+        map.put(10, "十");
+        map.put(11, "十一");
+        map.put(12, "十二");
+        map.put(13, "十三");
+        map.put(14, "十四");
+        map.put(15, "十五");
+        map.put(16, "十六");
+        map.put(17, "十七");
+        map.put(18, "十八");
+        map.put(19, "十九");
+        map.put(20, "二十");
+        map.put(21, "二十一");
+        map.put(22, "二十二");
+        map.put(23, "二十三");
+        map.put(24, "二十四");
+        map.put(25, "二十五");
+        map.put(26, "二十六");
+        map.put(27, "二十七");
+        map.put(28, "二十八");
+        map.put(29, "二十九");
+        map.put(30, "三十");
+        return map;
+    }
 }
--- src/main/java/namechange/excel/Read.java
+++ src/main/java/namechange/excel/Read.java
@@ -1,28 +1,63 @@
 package namechange.excel;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Read {
 
-    public static void read(String path) {
-
+    public static Map<String, List<ExcelModel>> readExcel(String path) {
         try {
             InputStream inputStream = new FileInputStream(path);
-            List<ExcelModel> modelList = EasyExcel.read(inputStream).head(ExcelModel.class).sheet().headRowNumber(1).doReadSync();
-
-
+            List<ExcelModel> excelModels = EasyExcel.read(inputStream).head(ExcelModel.class).sheet().headRowNumber(1).doReadSync();
+            if(CollectionUtils.isEmpty(excelModels)){
+                return new HashMap<>();
+            }
+            Map<String,List<ExcelModel>> nameToExcelModelList = new HashMap<>();
+            for (ExcelModel excelModel : excelModels) {
+                String name = excelModel.getName();
+                List<ExcelModel> excelModelList = nameToExcelModelList.getOrDefault(name, new ArrayList<>());
+                excelModelList.add(excelModel);
+                nameToExcelModelList.put(name,excelModelList);
+            }
+            return nameToExcelModelList;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        return new HashMap<>();
+    }
+
+    public static Map<String, List<File>> readImageFolder(String folderPath){
+        // 创建一个Map对象来存储图像文件
+        // 读取文件夹中的所有文件
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles();
+        if(Objects.isNull(files)){
+            return new HashMap<>();
+        }
+        // 遍历文件夹中的所有文件
+        Map<String, List<File>> userNameToFileListMap = new HashMap<>();
+        Map<String, File> fileNameToFileMap = Arrays.stream(files).filter(File::isFile).collect(Collectors.toMap(File::getName, t -> t));
+        for (Map.Entry<String, File> item : fileNameToFileMap.entrySet()) {
+            String[] s = item.getKey().split(" ");
+            String name = s[0];
+            List<File> fileList = userNameToFileListMap.getOrDefault(name, new ArrayList<>());
+            fileList.add(item.getValue());
+            userNameToFileListMap.put(name,fileList);
+        }
+        return userNameToFileListMap;
     }
 
 
     public static void main(String[] args) {
-        Read.read("/Users/chenzengsen09222/Desktop/excel.xls");
+        System.out.println(JSONObject.toJSONString(readExcel("C:\\Users\\bb135\\Desktop\\代发客户排序导单模板（吱吖）(1).xls")));
+        System.out.println(JSONObject.toJSONString(readImageFolder("C:\\Users\\bb135\\Desktop\\徐方宇 2.7")));
     }
 }
